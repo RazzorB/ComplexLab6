fdaee1eba6e1b70bf0e7fd61bf3179c9ecce4ded
RazzorB <razzorb@ya.ru>
2022-04-19 02:10:34 +0500

    testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include     "mycomplex.h"
+#include "mycomplex.h"
+
 using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
+int main() // управл€юща€ конструкци€ (заголовок функции main)
+{
+	//тестирование класса комплексных чисел
+	Complex A,
+		B(-4),
+		C(23.0, 45.9);
 
-    Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+	cout << A << "," << B << "," << C << endl << endl;
+	A = B + C;
+	cout << "A = B + C, A = " << A << endl;
+
+	Complex M = B - C;
+	cout << "M = B - C, M = " << M << endl;
+	cout << "M * A = " << M * A << endl;
+	cout << "M / 4.45 = " << M / 4.45 << endl << endl;
+
+	Complex D;
+	cout << "Enter complex number D = ";
+
+	cin >> D;
+	A += C + D;
+	cout << "D = " << D << endl;
+	cout << "A += C + D, A = " << A << endl;
+	return 0;
+}
